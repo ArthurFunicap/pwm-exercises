b71fcbef3c8634df90b9a6856897727df74bb2bd
ArthurFunicap <128716121+ArthurFunicap@users.noreply.github.com>
2025-08-19 12:00:12 +0000

    Atualização 2

--- ex01-next/app/page.js
+++ ex01-next/app/page.js
@@ -1,10 +1,27 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Stardos_Stencil } from "next/font/google";
 
 export default function Home() {
   return (
     <div>
-      <h1> Olá, Mundo! </h1>
+      <h1> Linguagem C </h1>
+
+      <h2> Funções: </h2>
+
+      <pre>
+        <code>
+          #include &lt;stdio.h&gt;
+          <br />
+          <br />
+          int main()&#x0007B;
+            <br />
+            &#9;&#9; int num1, num2, soma;
+            <br />
+
+          &#x0007D;
+        </code>
+      </pre>
     </div>
   );
 }
