4ddf328d47936598f544b8c73dfa5964e91bdf36
ArthurFunicap <128716121+ArthurFunicap@users.noreply.github.com>
2025-09-09 11:58:55 +0000

    init

--- ex01-next/app/page.js
+++ ex01-next/app/page.js
@@ -1,27 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { Stardos_Stencil } from "next/font/google";
+import MyButton from "@/components/MyButton";
 
 export default function Home() {
   return (
     <div>
-      <h1> Linguagem C </h1>
-
-      <h2> Funções: </h2>
-
-      <pre>
-        <code>
-          #include &lt;stdio.h&gt;
-          <br />
-          <br />
-          int main()&#x0007B;
-            <br />
-            &#9;&#9; int num1, num2, soma;
-            <br />
-
-          &#x0007D;
-        </code>
-      </pre>
+      <h1>Welcome to my app</h1>
+      <span className="colorText">Hello, World!</span>
+      <br />
+      <MyButton />
     </div>
   );
 }
