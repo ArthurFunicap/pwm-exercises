89f0a9abfdde7a3ce0ac9209bd996f86b6427881
ArthurFunicap <128716121+ArthurFunicap@users.noreply.github.com>
2025-09-10 21:14:55 +0000

    portifolioA4

--- portifolio/app/page.js
+++ portifolio/app/page.js
@@ -1,10 +1,20 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import "./App.css";
 
 export default function Home() {
   return (
-    <div id="portifolioPage">
-      
+    <div className="divContainer">
+      <div className="portifolioContainer">
+        <div className="divImage">
+          <Image className="portifolioImage" src="/assets/fotoPerfil.jpeg" alt="Foto de perfil" width={304} height={304} />
+        </div>
+        <div className="portifolioTitle">
+          <h2 className="portifolioTitleID">Arthur Felipe Leite de Vasconcelos</h2>
+        </div>
+        <div className="portifolioDescTitle">
+          <p>Estudante de Ciência da Computação</p>
+        </div>
+      </div>
     </div>
   );
 }
