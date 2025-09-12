c940fdfdad8dd93d82cef09cf05215a074b8aac0
ArthurFunicap <128716121+ArthurFunicap@users.noreply.github.com>
2025-09-12 00:59:29 +0000

    falta só o jogo

--- portifolio/app/page.js
+++ portifolio/app/page.js
@@ -23,7 +23,7 @@ export default function Home() {
         </div>
 
         <div className="portifolioMid">
-          <hr />
+          <hr className="linhaDivisora" />
 
           <div>
             <h1>Descrição:</h1>
@@ -31,6 +31,41 @@ export default function Home() {
 
           <div className="description">
             <p className="descriptionText">Olá, sou Arthur Felipe. Eu tenho interesse em Inteligência Artificial e Ciência de Dados e estou à procura de um estágio.</p>
+          </div>
+
+          <div className="certificatesTitle">
+            <h1>Certificados:</h1>
+          </div>
+
+          <div className="certificates">
+            <ul className="certificatesList">
+              <li>Fundamentos de IA - IBM SkillsBuild</li>
+              <li>Introdução à Ciência de Dados - Cisco</li>
+              <li>Defesa de Redes - Cisco</li>
+            </ul>
+          </div>
+
+          <div className="skillsTitle">
+            <h1>Habilidades:</h1>
+          </div>
+
+          <div className="skills">
+            <ul className="skillsList">
+              <li>Python - Intermediário</li>
+              <li>Bibliotecas Numpy e Pandas</li>
+              <li>Power BI - Básico</li>
+              <li>Excel - Intermediário</li>
+              <li>IBM Watson Studio - Básico</li>
+              <li>N8n - Básico</li>
+            </ul>
+          </div>
+
+          <hr className="linhaDivisora" />
+        </div>
+
+        <div className="portifolioBottom">
+          <div className="jogoDaForca">
+            
           </div>
         </div>
       </div>
